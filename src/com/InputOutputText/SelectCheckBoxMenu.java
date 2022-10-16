413b1e919ef0e75bc2ea97c9fb2e40a527562ee4
Sachin Savita <sachinsavita95@gmail.com>
2022-10-16 22:24:30 +0530

    More Fields Updated

---
+++ src/com/InputOutputText/SelectCheckBoxMenu.java
@@ -0,0 +1,10 @@
+package com.InputOutputText;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="SCBM")
+public class SelectCheckBoxMenu {
+	
+	
+
+}
--- src/com/InputOutputText/SelectOneRadioDemo.java
+++ src/com/InputOutputText/SelectOneRadioDemo.java
@@ -1,21 +1,24 @@
 package com.InputOutputText;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean(name="SOR")
 public class SelectOneRadioDemo {
 	
 	
 	private String option;
-
+	String test;
 	
 	public String getOption() {
 		return option;
-	} 
+	}
 
 	public void setOption(String option) {
+		
 		this.option = option;
-		System.out.println(option);
+		 
 		
 	}
 	
